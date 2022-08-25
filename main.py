f500b4baecf4926c373a62fcab97cf26fc905ce3
breogann <fernandocostapradillo@gmail.com>
2022-08-25 10:04:58 +0200

    uploading

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
 
 
 
@@ -33,12 +32,6 @@ hist_data = [x1, x2, x3]
 
 group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-         hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
 
 
 df2 = pd.DataFrame(
